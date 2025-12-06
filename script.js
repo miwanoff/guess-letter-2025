9b1b76c7a72371ffb2bbd3ddc41e7dc49d52564c
miwanoff <marina.iva.noffa@gmail.com>
2025-12-06 12:35:44 +0200

    Add guessLetter function

--- script.js
+++ script.js
@@ -8,8 +8,23 @@ answer[word.length - 1] = word[word.length - 1];
 for (let i = 1; i < word.length - 1; i++) {
   answer[i] = "_";
 }
-
+let guess = document.getElementById("guess");
 let answ = document.getElementById("answ");
 answ.innerHTML = answer.join(" ");
 
 console.log(answer.join(" "));
+
+function guessLetter() {
+  const letter = prompt("Вгадай літеру");
+
+  for (let i = 1; i < word.length - 1; i++) {
+    if (letter === word[i]) {
+      answer[i] = letter;
+    }
+  }
+
+  answ.innerHTML = answer.join(" ");
+}
+
+
+guess.addEventListener("click", guessLetter)
